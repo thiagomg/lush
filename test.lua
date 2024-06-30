75875fbd19128e05e053b2112b596b0a3ad8b994
Thiago Massari Guedes <thiedri@gmail.com>
2024-06-29 21:10:07 -0700

    Cleaning v1

--- test.lua
+++
@@ -1,8 +0,0 @@
-local cmdline = require("cmdline")
-local inspect = require("inspect")
-
--- Testing - for now only dumping to console
--- ./lush test.lua 1a --fk=1 -sk=a tk=3 2a --name me
-local options = cmdline:parse(arg)
-print(inspect(options))
-print('----------------------------------------')
